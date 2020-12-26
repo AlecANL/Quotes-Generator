76b656f0db3c8900d47ef8d6fd60e21ea9b1950f
alec <alexeialvarez4@gmail.com>
2020-12-25 22:09:40 -0600

    it's work was remake all app to add new ui features

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,85 +1,165 @@
 'use strict';
 
-const $quoteText = document.getElementById('quoteParagraph'),
-  $authorQuote = document.getElementById('authorQuote'),
-  $typeQuote = document.getElementById('typeQuote'),
-  $btnRandom = document.getElementById('btn-random'),
-  $loader = document.getElementById('loader'),
-  $btnAuthor = document.getElementById('btn-author'),
-  $modal = document.getElementById('modal'),
+const $quote = document.getElementById('quote'),
+  $author = document.getElementById('author'),
+  $typeQuote = document.getElementById('type'),
   $quoteContainer = document.getElementById('quote-container'),
-  $quote = document.querySelectorAll('.quote'),
-  $quotesByModal = document.querySelector('.quotes'),
-  $random = document.querySelector('.random'),
-  $close = document.querySelector('.close');
+  $allQuotes = document.getElementById('allquotes-container'),
+  $loader = document.getElementById('loader'),
+  $btnRandom = document.getElementById('btn-random'),
+  $btnClose = document.getElementById('btn-close'),
+  $btnAuthorSection = document.getElementById('btn-author-section'),
+  $modal = document.getElementById('modal');
 
-const api = 'https://quote-garden.herokuapp.com/api/v3/quotes/random';
-let loader = true;
+let isLoader = true;
 let author = undefined;
 $quoteContainer.style.display = 'none';
-$btnAuthor.style.display = 'none';
-$close.style.display = 'none';
 
+const api = 'https://quote-garden.herokuapp.com/api/v3/quotes/random';
+/**
+ *
+ * Request Data to API
+ */
+
+/**
+ *
+ * @param {*} urlAPI
+ */
 async function getRandomQuote(urlAPI) {
   const fetchQuote = await fetch(urlAPI);
   const getQuote = await fetchQuote.json();
   return getQuote;
 }
 
-async function renderDOM() {
-  const quote = await getRandomQuote(api);
-  $loader.style.display = 'none';
-  $quoteText.innerText = quote.quote.quoteText;
-  $authorQuote.innerText = quote.quote.quoteAuthor;
-  $typeQuote.innerText = quote.quote.quoteGenre;
-  $btnAuthor.style.display = 'block';
-  $quoteContainer.style.display = 'block';
-  author = quote.quote.quoteAuthor;
+/**
+ *
+ * @param {*} limit
+ */
+async function getQuoteByAuthor(limit = 10) {
+  isLoader = false;
+  const apiByAuthor = `https://quote-garden.herokuapp.com/api/v3/quotes?author=${author}&&limit=${limit}`;
+  const response = await getRandomQuote(apiByAuthor);
+  const data = response.data;
+  render(data, $allQuotes);
+  isVisible(isLoader);
 }
-function getQuoteByButton() {
-  $loader.style.display = 'block';
-  $btnAuthor.style.display = 'none';
-  $quoteContainer.style.display = 'none';
+
+/**
+ * Functions to util by load components
+ * @param {*} obj
+ * @param {*} $container
+ */
+function loadComponents(obj, $container) {
+  const $el = document.createElement('div');
+  $el.innerHTML = cardQuoteComponent(obj);
+  $container.appendChild($el.firstElementChild);
+}
+
+function getRandomQuoteByButton() {
+  isLoader = true;
+  isVisible(isLoader);
   renderDOM();
 }
 
 function showAllQuotes() {
+  isLoader = true;
   $loader.style.display = 'block';
-  $modal.classList.remove('hidden');
-  $random.style.display = 'none';
-  $close.style.display = 'block';
-  $quote.forEach(a => (a.style.display = 'none'));
+  showModal();
   getQuoteByAuthor();
-}
-async function getQuoteByAuthor() {
-  let apiByAuthor = `https://quote-garden.herokuapp.com/api/v3/authors/${author}?page=1&limit=10`;
-  const quoteByAuthor = await getRandomQuote(apiByAuthor);
-  loadComponent(quoteByAuthor.quotes);
-}
-function component({ quoteText }) {
-  return `
-    <p class="quote quote-container">${quoteText}</p>
-  `;
+  clearHTML($allQuotes);
 }
 
-function loadComponent(arrayQuotes) {
-  const el = document.createElement('div');
-  arrayQuotes.forEach(quote => {
-    const c = component(quote);
-    el.innerHTML = c;
-    $quotesByModal.appendChild(el.firstElementChild);
-  });
-  $loader.style.display = 'none';
-  $quote.forEach(a => (a.style.display = 'block'));
+/**
+ *
+ * Components
+ */
+
+async function singleCardComponent() {
+  const data = await getRandomQuote(api);
+  const quote = data.data[0];
+  $quote.textContent = quote.quoteText;
+  $author.textContent = quote.quoteAuthor;
+  $typeQuote.textContent = quote.quoteGenre;
+  author = quote.quoteAuthor;
+  isVisible(isLoader);
 }
 
-$btnRandom.addEventListener('click', getQuoteByButton);
-$btnAuthor.addEventListener('click', showAllQuotes);
-document.addEventListener('DOMContentLoaded', () => {
-  renderDOM();
-});
-$close.addEventListener('click', () => {
-  $modal.classList.add('hidden');
-  $close.style.display = 'none';
-  $random.style.display = 'block';
-});
+/**
+ *
+ * @param {*} param0
+ */
+function cardQuoteComponent({ quoteText }) {
+  return `<p class="quote all m-1" id="quote">${quoteText}</p>`;
+}
+
+/**
+ *
+ * Render Components
+ */
+
+/**
+ *
+ * @param {*} arr
+ * @param {*} $container
+ */
+function render(arr, $container) {
+  isLoader = false;
+  if (!isLoader) {
+    arr.forEach(el => loadComponents(el, $container));
+  }
+}
+
+function renderDOM() {
+  isLoader = false;
+  if (!isLoader) {
+    singleCardComponent();
+  }
+}
+
+/**
+ * Utils Functions
+ * */
+
+/**
+ *
+ * @param {*} $el
+ */
+function clearHTML($el) {
+  while ($el.firstChild) {
+    $el.removeChild($el.firstChild);
+  }
+}
+
+/**
+ *
+ * @param {*} hasLoader
+ */
+function isVisible(hasLoader) {
+  if (!hasLoader) {
+    $loader.style.display = 'none';
+    $quoteContainer.style.display = 'block';
+    $allQuotes.style.display = 'block';
+    return;
+  }
+  $loader.style.display = 'block';
+  $quoteContainer.style.display = 'none';
+  $allQuotes.style.display = 'none';
+}
+
+function showModal() {
+  if (!$modal.classList.contains('active')) {
+    $modal.classList.add('active');
+    return;
+  }
+  $modal.classList.remove('active');
+  isLoader = false;
+}
+
+function closeModal() {
+  showModal();
+}
+
+renderDOM();
+$btnRandom.addEventListener('click', getRandomQuoteByButton);
+$btnAuthorSection.addEventListener('click', showAllQuotes);
+$btnClose.addEventListener('click', closeModal);
